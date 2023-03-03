856772b413296d1462df8e1c5be6833215e689cd
Joycey <Jcheung8385@gmail.com>
2023-03-03 19:23:08 +0000

    Added LocationButton component and LocationButton CSS file.

---
+++ src/components/LocationButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./LocationButton.css";
+const LocationButton = () => {
+  return (
+    <button className="location-btn">
+      <i className="fa-solid fa-location-dot"></i>
+    </button>
+  );
+};
+
+export default LocationButton;
